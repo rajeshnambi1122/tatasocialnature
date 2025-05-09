0eedf4758203995113b5f34adcf75a1eef740195
rajeshnambi1122 <rajeshnambi2016@gmail.com>
2025-05-09 21:51:20 +0530

    gallery carousel design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,11 +13,26 @@ export class HomeComponent implements OnInit {
   isMobileMenuOpen = false;
   currentSlideIndex = 0;
   tirunelveliSlideIndex = 0;
+  gallerySlideIndex = 0;
   
   carouselImages = [
     { src: 'images/gallery1.jpg', alt: 'Marathon Event' },
     { src: 'images/gallery2.jpg', alt: 'Runners in Action' },
     { src: 'images/gallery3.jpg', alt: 'Marathon Participants' }
+  ];
+  
+  gallerySectionImages = [
+    { src: 'images/gallery1.jpg', alt: 'Gallery Image 1' },
+    { src: 'images/gallery2.jpg', alt: 'Gallery Image 2' },
+    { src: 'images/gallery3.jpg', alt: 'Gallery Image 3' },
+    { src: 'images/gallery4.jpg', alt: 'Gallery Image 4' },
+    { src: 'images/gallery5.jpg', alt: 'Gallery Image 5' },
+    { src: 'images/gallery6.jpg', alt: 'Gallery Image 6' },
+    { src: 'images/gallery7.jpg', alt: 'Gallery Image 7' },
+    { src: 'images/gallery8.jpg', alt: 'Gallery Image 8' },
+    { src: 'images/gallery9.jpg', alt: 'Gallery Image 9' },
+    { src: 'images/gallery10.jpg', alt: 'Gallery Image 10' },
+    { src: 'images/gallery11.jpg', alt: 'Gallery Image 11' }
   ];
   
   tirunelveliImages = [
@@ -40,6 +55,8 @@ export class HomeComponent implements OnInit {
   startAutoSlide() {
     this.autoSlideInterval = setInterval(() => {
       this.nextSlide();
+      this.nextTirunelveliSlide();
+      this.nextGallerySlide();
     }, 5000);
   }
   
@@ -74,6 +91,17 @@ export class HomeComponent implements OnInit {
   
   prevTirunelveliSlide() {
     this.tirunelveliSlideIndex = (this.tirunelveliSlideIndex - 1 + this.tirunelveliImages.length) % this.tirunelveliImages.length;
+    // Reset auto-slide timer when manually changing slides
+    clearInterval(this.autoSlideInterval);
+    this.startAutoSlide();
+  }
+  
+  nextGallerySlide() {
+    this.gallerySlideIndex = (this.gallerySlideIndex + 1) % this.gallerySectionImages.length;
+  }
+  
+  prevGallerySlide() {
+    this.gallerySlideIndex = (this.gallerySlideIndex - 1 + this.gallerySectionImages.length) % this.gallerySectionImages.length;
     // Reset auto-slide timer when manually changing slides
     clearInterval(this.autoSlideInterval);
     this.startAutoSlide();
